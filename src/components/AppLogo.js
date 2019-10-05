6b1796921bac9aa35f5feca36b5f70e8c5999e2c
Poe <piotr.a.ptak@icloud.com>
2019-10-06 01:46:47 +0200

    Implement new logo.
    - create Letter component with color swapping

--- src/components/AppLogo.js
+++ src/components/AppLogo.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import Letter from './Letter';
 
 const Wrapper = styled.div`
   display: block;
   font-size: 50px;
+  letter-spacing: 10px;
 `;
 
 const AppLogo = () => (
-  <Wrapper>spockoko</Wrapper>
+  <Wrapper>
+    {'rpslstw'.split('').map((letter, index) => <Letter key={letter + '-' + index}>{letter}</Letter>)}
+  </Wrapper>
 );
 
 export default AppLogo;
---
+++ src/components/Letter.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { seconds } from '../helpers/time';
+
+const colors = [
+  '#C0B9DD',
+  '#80A1D4',
+  '#75C9C8',
+  '#2D2C2D'
+];
+
+const TICK_SPEED = seconds(.12);
+
+const getRandomColor = () => colors[Math.floor(colors.length * Math.random(1))];
+
+export default class Letter extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      stabilizeColor: seconds(Math.ceil(5 * Math.random(1))),
+      color: getRandomColor(),
+    };
+
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    this.tickInterval = setInterval(() => {
+      this.tick();
+    }, TICK_SPEED);
+  }
+
+  tick() {
+    const { stabilizeColor } = this.state;
+
+    this.setState({
+      stabilizeColor: stabilizeColor - TICK_SPEED,
+      color: getRandomColor(),
+    }, () => {
+      if (stabilizeColor <= seconds(1)) {
+        clearInterval(this.tickInterval);
+      }
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { color } = this.state;
+
+    return (
+      <span style={{ color: color }}>
+        {children}
+      </span>
+    )
+  }
+}
+
+Letter.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+};
+
+Letter.defaultProps = {
+  children: [],
+};
+
